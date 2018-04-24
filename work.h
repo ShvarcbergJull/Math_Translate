effa6f6cf20adbc05c603d497bfd98452dfbf639
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2018-04-24 23:02:12 +0800

    Create work.h

---
+++ work.h
@@ -0,0 +1,18 @@
+#ifndef _WORK_
+#define _WORK_
+
+struct Chisla
+{
+	int i;
+	char c;
+};
+
+void Zap(Chisla* c);
+
+int Size(char* c);
+
+void arabRim(int m, Chisla* ch);
+
+void rimArab(char* a, Chisla* ch);
+
+#endif // !_WORK_
